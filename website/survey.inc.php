72a3129e4d9de85942cdd088746cfb901d0a4e38
Griffin Clark <32806921+GriffClark@users.noreply.github.com>
2020-11-08 18:24:41 -0800

    moved app to other git repo

--- website/survey.inc.php
+++ website/survey.inc.php
@@ -21,25 +21,35 @@ if (isset($_POST["submit"])) {
     while ($row = mysqli_fetch_array($resultData)) {
 
         $userId = $_SESSION['userId'];
-        $id = $row['id'];
-        $answer = ($_POST["question$id"]);
+        $rowId = $row['id'];
+        $answer = ($_POST["question$rowId"]);
         
         // DEBUG
-        echo "for user $userId @ question$id, answer $answer ";
+        // echo "for user $userId @ question$rowId, answer $answer ";
 
         $sql = "INSERT INTO answers (userId, questionId, answer) VALUES (?,?,?);";
-        // $sql = "INSERT INTO users (usersFullName, usersEmail, usersPwd) VALUES (?,?,?);";
         $stmt = mysqli_stmt_init($conn);
 
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("location: index.php?error=stmtFailed");
             exit();
         }
-        mysqli_stmt_bind_param($stmt, "sss",$userId, $id, $answer);
+        mysqli_stmt_bind_param($stmt, "sss",$userId, $rowId, $answer);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
 
     }
+
+    // Make sure we add our user to the dirtyUser table
+    $sql = "INSERT INTO dirtyusers (userId) VALUES (?);"; //TODO: clean
+    $stmt = mysqli_stmt_init($conn); 
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: index.php?error=dirtyTableFail");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "s",$userId); 
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
 
     header("location: index.php?error=none");
     exit();
